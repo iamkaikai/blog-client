163f6dd428c0a12cb5848531d0f61429b64c2653
YENKAI HUANG <iamkaikai@livemail.tw>
2023-05-17 19:29:01 -0400

    empty state update

--- src/components/fetchPosts.js
+++ src/components/fetchPosts.js
@@ -12,6 +12,7 @@ import optionEdit from '../img/edit.svg';
 function Posts() {
     const dispatch = useDispatch();
     const posts = useSelector((store) => { return store.posts.all; });
+
     useEffect(() => {
         dispatch(fetchAll());
     }, []);
@@ -30,55 +31,61 @@ function Posts() {
     const handleClick = (event, postID) => {
         setVisiblePostId(visiblePostId === postID ? null : postID);
     };
-
-    return (
-
-        <div className="postContainer">
-            <NavLink to="/posts/new"><img src={createBTN} alt="" className="createBTN" /></NavLink>
-            { posts.map((post) => {
-                return (
-                    <div className="post" key={post.id}>
-                        <Link to={`/posts/${post.id}`}>
-                            <button type="button" className="postImgButton" onClick={() => detail(post.id)}>
-                                <div className="postImg">
-                                    {
-                                        post.coverUrl.startsWith('http')
-                                            ? <img src={post.coverUrl} alt={post.title} />
-                                            : <h1>{post.title}</h1>
-                                    }
-                                </div>
-                            </button>
-                        </Link>
-                        <img className="optionIcon" onClick={(event) => handleClick(event, post.id)} src={optionBTN} id={post.id} alt="" />
-                        <h3 onClick={() => { detail(post.id); }}>{post.title}</h3>
-                        <h3 className="tags">tags: {post.tags}</h3>
-
-                        <Link to={`/posts/edit/${post.id}`}>
-                            <img src={optionEdit}
-                                className="optionIcon"
-                                id="OptionEdit"
-                                onClick={() => { detail(post.id); }}
+    if (!posts || posts.length === 0) {
+        return (
+            <div className="postContainer">
+                <NavLink to="/posts/new"><img src={createBTN} alt="" className="createBTN" /></NavLink>
+                <div className="empty_title">no post to display yet</div>
+                <div className="empty_content">To create a new post, please click on the button at the bottom-right cornerf</div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="postContainer">
+                <NavLink to="/posts/new"><img src={createBTN} alt="" className="createBTN" /></NavLink>
+                { posts.map((post) => {
+                    return (
+                        <div className="post" key={post.id}>
+                            <Link to={`/posts/${post.id}`}>
+                                <button type="button" className="postImgButton" onClick={() => detail(post.id)}>
+                                    <div className="postImg">
+                                        {
+                                            post.coverUrl.startsWith('http')
+                                                ? <img src={post.coverUrl} alt={post.title} />
+                                                : <h1>{post.title}</h1>
+                                        }
+                                    </div>
+                                </button>
+                            </Link>
+                            <img className="optionIcon" onClick={(event) => handleClick(event, post.id)} src={optionBTN} id={post.id} alt="" />
+                            <h3 onClick={() => { detail(post.id); }}>{post.title}</h3>
+                            <h3 className="tags">tags: {post.tags}</h3>
+                            <Link to={`/posts/edit/${post.id}`}>
+                                <img src={optionEdit}
+                                    className="optionIcon"
+                                    id="OptionEdit"
+                                    onClick={() => { detail(post.id); }}
+                                    style={{
+                                        display: visiblePostId === post.id ? 'block' : 'none',
+                                    }}
+                                    alt=""
+                                />
+                            </Link>
+                            <img className="optionIcon"
+                                id="OptionDel"
+                                src={optionDel}
+                                onClick={() => { del(post.id); }}
                                 style={{
                                     display: visiblePostId === post.id ? 'block' : 'none',
                                 }}
                                 alt=""
                             />
-                        </Link>
-
-                        <img className="optionIcon"
-                            id="OptionDel"
-                            src={optionDel}
-                            onClick={() => { del(post.id); }}
-                            style={{
-                                display: visiblePostId === post.id ? 'block' : 'none',
-                            }}
-                            alt=""
-                        />
-                    </div>
-                );
-            })}
-        </div>
-    );
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
 }
 
 export default Posts;
